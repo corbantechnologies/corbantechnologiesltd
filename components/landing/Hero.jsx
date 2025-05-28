e9f1b2da83dcfe61b6a44127fbee956804d3aebf
Dalienst Owino <oduordalienst@gmail.com>
2025-05-28 23:05:16 +0300

    completed hero section

--- components/landing/Hero.jsx
+++ components/landing/Hero.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function Hero() {
   return (
-    <div className="h-96 grid sm:grid-cols-2 p-4 items-center">
+    <div className="grid sm:grid-cols-2 gap-4 px-4 py-12 items-center bg-black text-white bg-gradient-to-b from-black to-gray-900">
+      <div>
+        <h1 className="text-5xl font-bold mb-4">
+          Empowering lives through innovation
+        </h1>
+        <p className="mb-4">
+          Building a secure and innovative technology ecosystem that empowers
+          homes and businesses, driving growth and efficiency through smart
+          solutions.
+        </p>
+        <div className="flex gap-4">
+          <Link href="/" className="bg-[var(--maincolor)] rounded-md p-2">
+            Let's Talk →
+          </Link>
+          <button className="text-[var(--maincolor)]">Explore More →</button>
+        </div>
+      </div>
 
+      <div>
+        <Image src="/lady.jpg" className="w-full rounded-md" width={500} height={500} alt="hero" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
