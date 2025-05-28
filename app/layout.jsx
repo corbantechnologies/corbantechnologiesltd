b4321a0c8b271ed17f7eae65686630dd05fc770b
Dalienst Owino <oduordalienst@gmail.com>
2025-05-28 21:40:37 +0300

    setup

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Corban Technologies LTD</title>
+        <meta
+          name="description"
+          content="Building a secure and innovative technology ecosystem that empowers homes and businesses, driving growth and efficiency through smart solutions."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
