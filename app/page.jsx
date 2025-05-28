7f79be00fd4b8c4705079977a3322885d73eec53
Dalienst Owino <oduordalienst@gmail.com>
2025-05-28 23:45:04 +0300

    complete services

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Hero from "@/components/landing/Hero";
+import Services from "@/components/landing/Services";
 import React from "react";
 
 function LandingPage() {
   return (
     <>
       <Hero />
+      <Services />
     </>
   );
 }
